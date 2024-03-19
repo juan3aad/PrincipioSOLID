0567b2d48911c53501d46277281878a8c28f314d
Juan Alexander Agreda <juan3aad@gmail.com>
2024-03-18 23:45:43 -0500

    Realizando cambios

--- Ejercicio4_ArrayBien.java
+++ Ejercicio4_ArrayBien.java
@@ -2,20 +2,20 @@ public class Ejercicio4_ArrayBien {
 
     public class Main {
         public static void main(String[] args) {
-            String[] fruits = initializeFruits();
-            printFruits(fruits);
+            String[] fruitNames = initializeFruitNames();
+            printFruitNames(fruitNames);
         }
 
         // Método para inicializar el array de frutas
-        private static String[] initializeFruits() {
+        private static String[] initializeFruitNames() {
             return new String[] { "manzana", "banano", "fresa" };
         }
 
         // Método para imprimir las frutas
-        private static void printFruits(String[] fruits) {
-            System.out.println("Lista de frutas:");
-            for (String fruit : fruits) {
-                System.out.println(fruit);
+        private static void printFruitNames(String[] fruitNames) {
+            System.out.println("Lista de nombres de frutas:");
+            for (String fruitName : fruitNames) {
+                System.out.println(fruitName);
             }
         }
     }
